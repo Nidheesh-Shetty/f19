0bd3866eaf9d9705b30b78aa9d79720ab90c7a8d
GlitchedParadox <paradoxboixd@gmail.com>
2025-12-11 11:38:12 +0800

    login + register and wepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,44 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-green-300">
-      
-    </div>
+    <>
+      <header>
+        <div className="container p-3  flex items-center justify-between">
+          <img src="/images/logo.png" className="w-32 h-21"></img>
+
+
+
+          <nav>
+            <ul className="flex items-center gap-4">  
+              <li className="hover:font-bold">
+                <a href="/login"> Login</a>
+              </li>
+              <li className="hover:font-bold">
+                <a href="/register"> Register!</a>
+              </li>
+              
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <section
+          id="hero"
+          className=" relative bg-cover min-h-[384px] text-white flex justify-center items-center text-center"
+        >
+
+          <div className="absolute inset-0 z-0 bg-gray-800/45"></div>
+
+          <div className="container relative z-10">
+            <h1 className="text-7xl font-black uppercase ">Title goes here</h1>
+            <h2>Lorem ipsum</h2>
+            <a href="#" className="button"> Call to action </a>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
